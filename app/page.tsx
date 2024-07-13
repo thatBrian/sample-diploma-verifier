a65670fe962dfea5c4503184d822485d96cad901
thatbrian <yes229@nyu.edu>
2024-07-13 16:06:24 -0400

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,149 @@
-import Image from "next/image";
+'use client'
+import { CircleCheck, CircleX } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [fileName, setFileName] = useState('');
+
+  let content;
+
+  switch (currentStep) {
+    case 0:
+      content = (
+        <>
+          <p className="py-6">
+            Upload your provided PDF Diploma below. We will calculate the hash of the PDF and retrieve the metadata through our smart contract. We'll use the URL provided in the metadata to obtain the current public key and verify the signature.
+          </p>
+          <input type="file" className="file-input file-input-bordered w-full max-w-xs"
+            onChangeCapture={(event) => {
+              setFileName(event.target.files[0].name)
+              setCurrentStep(1);
+            }} />
+        </>
+      )
+      break;
+    case 1:
+      content = (
+        <>
+          <p className="py-6">
+            Processing PDF
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 2:
+      content = (
+        <>
+          <p className="py-6">
+            Computing the SHA-256 hash of the provided PDF.
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 3:
+      content = (
+        <>
+          <p className="py-6">
+            Querying smart contract with 'getCertificate' function.
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 4:
+      content = (
+        <>
+          <p className="py-6">
+            Obtained metadata. Querying metadata provided URL.
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 5:
+      content = (
+        <>
+          <p className="py-6">
+            Obtained current public key from metadata URL.
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 6:
+      content = (
+        <>
+          <p className="py-6">
+            Authenticating certificate with publickey.
+          </p>
+          <span className="loading loading-spinner loading-lg"></span>
+        </>
+      )
+      break;
+    case 7:
+      content = (
+        <>
+          <p className="py-6">
+            Authenticated! The provided diploma is authentic.
+          </p>
+          <p className="flex items-center justify-center">
+            <CircleCheck size={45} color={"#4BB543"} />
+          </p>
+          <button className="btn btn-neutral mt-8" onClick={() => setCurrentStep(0)}>Return to home</button>
+        </>
+      )
+      break;
+    case 8:
+      content = (
+        <>
+          <p className="py-6">
+            Authentication failed! The provided diploma is not authentic.
+          </p >
+          <p className="flex items-center justify-center">
+            <CircleX size={45} color={"#cc0000"} />
+          </p>
+          <button className="btn btn-primary mt-8" onClick={() => setCurrentStep(0)} >Return to hom</button>
+        </>
+      )
+      break;
+  }
+
+  useEffect(() => {
+    switch (currentStep) {
+      case 1:
+      case 2:
+        setTimeout(() => {
+          setCurrentStep(currentStep + 1);
+        }, 1000);
+        break;
+      case 3:
+      case 4:
+      case 5:
+        setTimeout(() => {
+          setCurrentStep(currentStep + 1);
+        }, 1500);
+        break;
+      case 6:
+        if (["real", "authentic"].some(names => fileName.toLocaleLowerCase().includes(names))) {
+          setCurrentStep(7);
+        } else {
+          setCurrentStep(8);
+        }
+      //Check file name.
+    }
+  }, [currentStep])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold"><span className="font-extrabold" style={{ color: "#56018D" }}>NYU</span> Diploma Verification</h1>
+          {content}
+        </div >
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
